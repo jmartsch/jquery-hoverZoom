c91f95a7af8ad85c921334d7ca4738717e99563a
Jens Martsch <jmartsch@gmail.com>
2011-01-17 10:34:17 +0100

    First stable version

--- jquery.hoverZoom.js
+++ jquery.hoverZoom.js
@@ -1,5 +1,4 @@
 (function($) {
-<<<<<<< HEAD:jquery.hoverZoom.js
 /**
 * hoverZoom // jQuery 1.4+
 * demoURL
@@ -18,8 +17,6 @@
 */
 (function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY;};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev]);}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob);},cfg.interval);}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev]);};var handleHover=function(e){var p=(e.type=="mouseover"?e.fromElement:e.toElement)||e.relatedTarget;while(p&&p!=this){try{p=p.parentNode;}catch(e){p=this;}}if(p==this){return false;}var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);}if(e.type=="mouseover"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob);},cfg.interval);}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob);},cfg.timeout);}}};return this.mouseover(handleHover).mouseout(handleHover);};})(jQuery);
 
-=======
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
     jQuery.extend(jQuery.easing, {
 	easeOutBack: function (x, t, b, c, d, s) {
 	    if (s == undefined) s = 1.70158;
@@ -40,27 +37,18 @@
 
     var methods = {
 	init: function(options){
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    var largeImg = false, caption;
-=======
-	    var largeImg = false, title;
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    var defaults = {
 		speedView: 200,
 		speedRemove: 400,
 		altAnim: true,
-<<<<<<< HEAD:jquery.hoverZoom.js
 		speedCaption: 400,
-=======
-		speedTitle: 400,
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 		debug: false,
 		ajaxLoader : 'ajax-loader.gif',
 		width: 600,
 		height: 400,
 		easing: 'easeOutQuint',
-<<<<<<< HEAD:jquery.hoverZoom.js
 		captionHeight: 32,
 		breathingSize: 0,
 		hoverIntent: true
@@ -71,20 +59,6 @@
 		    log('speedRemove: ' + opts.speedRemove);
 		    log('altAnim: ' + opts.altAnim);
 		    log('speedCaption: ' + opts.speedCaption);
-=======
-		titleHeight: 32,
-		breathingSize: 0
-	    };
-	    opts = $.extend(defaults, options);
-
-	    if (opts.debug === true) {
-		log(opts);
-		    log('speedView: ' + opts.speedView);
-		    log('speedRemove: ' + opts.speedRemove);
-		    log('altAnim: ' + opts.altAnim);
-		    log('speedTitle: ' + opts.speedTitle)
-	    }
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    return this.each( function(){
                 var original = $(this);
@@ -99,13 +73,7 @@
 		original.data('originalLeft',original.position().left);
 		original.data('originalTop',original.position().top);
 
-<<<<<<< HEAD:jquery.hoverZoom.js
 		// if the zoomContainer doesn´t exist yet, then create it
-=======
-		log('originalWidth: ' + original.data('originalWidth'));
-		log('originalHeight: ' + original.data('originalHeight'));
-                // if the zoomContainer doesn´t exist yet, then create it
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
                 if (!original.parent().parent().hasClass('zoomContainer')){
 		    var zoomContainer = original.parent().wrap('<div class="zoomContainer" />');
 		    var trigger = original.clone().appendTo(zoomContainer.parent().parent()).addClass('trigger');
@@ -115,13 +83,8 @@
                 }
 
 		var alt = original.attr('alt');
-<<<<<<< HEAD:jquery.hoverZoom.js
 		var caption = zoomContainer.find('.gallerycaption');
 		if (caption.length == 0) caption = $('<div class="gallerycaption">' + alt + '</div>').appendTo(zoomContainer);
-=======
-		var title = zoomContainer.find('.gallerytitle');
-		if (title.length == 0) title = $('<div class="gallerytitle">' + alt + '</div>').appendTo(zoomContainer);
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 		// remove alt attribute for both images because IE displays ugly tooltips
 		original.attr('alt','');
 		trigger.attr('alt','');
@@ -131,45 +94,27 @@
 
 		// Das ganze kann man auch ohne die if Abfrage mit einem Shorthand machen
 		// (typeof $.fn.hoverIntent == "function") ? true : false;
-<<<<<<< HEAD:jquery.hoverZoom.js
 		if (typeof $.fn.hoverIntent == "function" && opts.hoverIntent == true){
 		    trigger.css({'z-index':'110','opacity':'0'}).hoverIntent(function() {
 			//if (opts.debug == true) window.console.group(original);
 			//if (typeof console != "undefined" && typeof console.debug != "undefined") window.console.clear();
 			log('mouseenter on: '+original.attr('src'),'info');
-=======
-		if (typeof $.fn.hoverIntent == "function"){
-		    trigger.css({'z-index':'110','opacity':'0'}).hoverIntent(function() {
-			//if (opts.debug == true) window.console.group(original);
-			log('mouseenter on: '+original.attr('src'));
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 			original.data('zoomedOut',false);
 			$.fn.hoverZoom('loadImage',original,trigger,zoomContainer);
 
 		    }, function() {
-<<<<<<< HEAD:jquery.hoverZoom.js
 			log('mouseleave on: '+original.attr('src'),'info');
-=======
-			log('mouseleave on: '+original.attr('src'));
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 			$.fn.hoverZoom('zoomOut',original,trigger,zoomContainer);
 			//if (opts.debug == true) window.console.groupEnd()
 		    }).click(function(e){e.preventDefault();return false;});
 		}
 		else{
 		    trigger.css({'z-index':'110','opacity':'0'}).hover(function() {
-<<<<<<< HEAD:jquery.hoverZoom.js
 			//if (typeof console != "undefined" && typeof console.debug != "undefined") window.console.clear();
 			log('mouseenter on: '+original.attr('src'),'info');
 			$.fn.hoverZoom('loadImage',original,trigger,zoomContainer);
 		    }, function() {
 			log('mouseleave'+original.attr('src'),'info');
-=======
-			log('mouseenter on: '+original.attr('src'));
-			$.fn.hoverZoom('loadImage',original,trigger,zoomContainer);
-		    }, function() {
-			log('mouseleave'+original.attr('src'));
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 			$.fn.hoverZoom('zoomOut',original,trigger,zoomContainer);
 		    }).click(function(e){e.preventDefault();return false;});
 		}
@@ -179,13 +124,8 @@
 	},
 
 	loadImage: function(original,trigger,zoomContainer){
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    log('function loadImage()','info');
 	    if (original.parent('a').attr('href') && original.data('isLoaded') != true){
-=======
-	    if (opts.debug == true) log('function loadImage()');
-	    if (opts.preload && original.parent('a').attr('href') && original.data('isLoaded') != true){
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 		original.largeImgSrc = original.parent('a').attr('href');
 		log('largeImg that should be loaded: '+original.largeImgSrc);
@@ -219,7 +159,6 @@
 	    }
 	},
 	startZoom: function(original,trigger,zoomContainer){
-<<<<<<< HEAD:jquery.hoverZoom.js
 
 	    log('function startZoom()','info');
 	    var width, height;
@@ -229,15 +168,6 @@
 	    caption = zoomContainer.find('.gallerycaption');
 	    var dimensions = $.fn.hoverZoom('getDimensions',caption,zoomContainer,width,height);
 	    log('returned dimensions: '+dimensions);
-=======
-	    zoomContainer.addClass('zoomed');
-	    var width, height;
-	    if (width == undefined) width = original.data('loadedWidth');
-	    if (height == undefined) height = original.data('loadedHeight');
-	    title = zoomContainer.find('.gallerytitle');
-	    var dimensions = $.fn.hoverZoom('getDimensions',title,zoomContainer,width,height);
-	    log('dimensions: '+dimensions);
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    original.data('marginTop',0);
 	    original.data('marginLeft',0);
@@ -262,11 +192,7 @@
 		height: dimensions[1] + 'px',
 		marginTop: original.data('marginTop') + 'px',
 		marginLeft: original.data('marginLeft') + 'px'
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    }, opts.speedView+20,opts.easing, function(){
-=======
-	    }, opts.speedView,opts.easing, function(){
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 		//log('zoomcontainer width: '+zoomContainer.outerWidth());
 	    });
 
@@ -274,7 +200,6 @@
 		width: dimensions[0] +'px',
 		height: dimensions[1] + 'px'
 	    }, opts.speedView,opts.easing,function(){
-<<<<<<< HEAD:jquery.hoverZoom.js
 		if(caption.text() != '' && opts.altAnim == true) $.fn.hoverZoom('animateCaption',zoomContainer,dimensions);
 	    });
 	},
@@ -300,55 +225,21 @@
 	getViewport: function(){
 	    log('function getViewport()','info');
 	    $('body').data('viewport',[$(window).width(), $(window).height(), $(document).scrollLeft(), $(document).scrollTop() ]);
-=======
-		if(title.text() != '') $.fn.hoverZoom('animateTitle',zoomContainer,dimensions);
-	    });
-	},
-	animateTitle: function(zoomContainer,dimensions){
-	    var titlepos;
-	    if (opts.altAnim == true && title != undefined) {
-		log('function animateTitle()','info');
-                title = zoomContainer.find('.gallerytitle');
-		titleHeight = title.height();
-		titlepos = dimensions[1] - titleHeight;
-		title.css({
-		    'top': titlepos + 'px',
-		    'z-index' : '101',
-		    'width' : '100%'
-		}).show();
-		newTitlepos = titleHeight+10;
-		zoomContainer.animate({'height': '+='+newTitlepos+'px'},opts.speedTitle,opts.easing);
-		title.animate({'top': '+='+newTitlepos+'px'},opts.speedTitle,opts.easing);
-            }
-	},
-	getViewport: function(){
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 	    return [$(window).width(), $(window).height(), $(document).scrollLeft(), $(document).scrollTop() ];
 	},
         centerImage: function(original,zoomContainer,width,height){
             // calculate left and top position of the zoomed image
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    log('function centerImage()','info');
 	    var viewport = $('body').data('viewport');
-=======
-	    var viewport = $.fn.hoverZoom('getViewport');
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    original.data('itemLeft', width > viewport[0] ? viewport[2] : (viewport[2] + Math.round((viewport[0] - width) / 2)));
 	    original.data('itemTop', height > viewport[1] ? viewport[3] : (viewport[3] + Math.round((viewport[1] - height) / 2)));
 	    log('itemLeft: ' + original.data('itemLeft'));
 	    log('itemTop: ' + original.data('itemTop'));
         },
-<<<<<<< HEAD:jquery.hoverZoom.js
 	getDimensions: function(caption,zoomContainer,width,height){
 	    log('function getDimensions()','info');
 
-=======
-	getDimensions: function(title,zoomContainer,width,height){
-	    log('function getDimensions()','info');
-
-
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 	    var viewport = $.fn.hoverZoom('getViewport');
 
 	    // Wenn viewport 545 x 370 ist, dann passt es überhaupt nicht
@@ -363,7 +254,6 @@
 	    var winProportion = viewport[0] / viewport[1];
 
 
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    // Check if a caption exists
 	    // and assign it´s height
 	    var tt = caption.text()
@@ -385,34 +275,6 @@
 		var maxHeight = viewport[1] - captionHeight - bh - captionAdd;
 		var maxWidth = Math.round(maxHeight * imageProportion);
 		log('calculate max height based on page height');
-=======
-	    // Check if a title exists
-	    // and assign its height
-	    var tt = title.text()
-	    var titleAdd = 0;
-
-	    if(tt == '')  var titleHeight = 0;
-	    else{
-		var titleHeight = title.outerHeight();
-		var titleAdd = bh/2;
-	    }
-
-	    if (imageProportion > winProportion) {
-		// calculate max width base on page width
-		var maxWidth = viewport[0] - bw * 2 - opts.breathingSize * 2;
-		var maxHeight = Math.round(maxWidth / imageProportion);
-		log('// calculate max width base on page width')
-		log(imageProportion);
-		log('maxWidth berechnet' + maxWidth);
-		log('maxHeight berechnet' + maxHeight);
-	    } else {
-		// calculate max height base on page height
-		var maxHeight = viewport[1] - titleHeight - bh - titleAdd;
-		var maxWidth = Math.round(maxHeight * imageProportion);
-		log('// calculate max height base on page height')
-		log('maxWidth berechnet' + maxWidth);
-		log('maxHeight berechnet' + maxHeight);
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 	    }
 
 	    // Wenn Bild größer ist als Viewport
@@ -422,29 +284,15 @@
 	    }
 
 	    log('Viewport: ' + viewport);
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    log('maxWidth: ' + maxWidth + ', maxHeight: '+maxHeight);
 	    finalHeight = (height + bh + captionHeight + captionAdd + opts.breathingSize * 2);
 	    finalWidth = (width + bw + opts.breathingSize * 2);
 	    log('final width and height of the container: ' + finalWidth +',' + finalHeight);
-=======
-	    finalHeight = (height + bh + titleHeight + titleAdd + opts.breathingSize * 2);
-	    finalWidth = (width + bw + opts.breathingSize * 2);
-	    log('Breite und Höhe inkl. Ränder: ' + finalWidth +',' + finalHeight);
-	    //log('Breite und Höhe skaliertes Bild: ' + maxWidth + ',' + maxHeight);
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    return [width,height,finalWidth,finalHeight];
 	},
 	zoomOut: function(original,trigger,zoomContainer){
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    log('function zoomOut','info');
-=======
-	    log('function zoomOut');
-
-	    // Diese Abfrage entfernen und statt trigger als Parameter der Funktion übermitteln
-	    //var zoomContainer = original.parent().parent();
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    // restore thumbnail src
 	    original.attr('src',original.data('image'));
@@ -452,13 +300,8 @@
 	    // Remove the loading indicator
 	    original.next('.loading').remove();
 
-<<<<<<< HEAD:jquery.hoverZoom.js
 	    // Remove the caption
 	    zoomContainer.find('.gallerycaption').css('display','none');
-=======
-	    // Remove the title
-	    zoomContainer.find('.gallerytitle').hide();
->>>>>>> 3e3879dfd3950c753279a5d9e93e945cf43bb245:jquery.hoverZoom.js
 
 	    if (zoomContainer.hasClass('zoomed')) zoomContainer.css({'z-index': '0'}).removeClass("zoomed").stop(true,false).animate({
 		marginTop: '0',
